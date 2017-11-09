3a6485374114a874cfb594da83e70c697ac26cbb
pekarsky.maxim@gmail.com <flufci@gmail.com>
2017-11-09 18:10:32 -0500

    do not log metrics page visits

--- app/dbops.js
+++ app/dbops.js
@@ -1001,7 +1001,7 @@ function logVisit(db, req, callback){
     }
 
 
-    if(req.url != "/get-definitions" && req.url != "/search"){
+    if(req.url != "/get-definitions" && req.url != "/search" && req.url != "/metrics"){
 
     	console.log("userIP: " + userIP);
 
