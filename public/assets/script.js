7857a709519391d8c5ec59554de20b8901196ca4
pekarsky.maxim@gmail.com <flufci@gmail.com>
2017-11-14 17:36:27 -0500

    console log mobile key code

--- public/assets/script.js
+++ public/assets/script.js
@@ -275,11 +275,11 @@ function main(){
     });
 
     $("#search-bar").on("keyup", function(e){
-        $("#main-section").prepend("keyup<br>");
         if($("#search-bar").val().length > 2){
-            $("#main-section").prepend("over 2<br>");
+            $("#main-section").prepend(e.which + "<br>");
+
             if((e.which >= 48 && e.which <= 90) || (e.which >= 106 && e.which <= 111) || (e.which >= 186 && e.which <= 192) || e.which == 8){       // 48-90 are letters and numbers 
-                $("#main-section").prepend("prepping for search<br>");
+
                 logSearch();
                 search();
             }
